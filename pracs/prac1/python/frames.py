3d069898b2a6ff7b3cfa76835322c7b069d1e346
joanjajas <joanbeltranperis@gmail.com>
2024-02-28 10:28:05 +0100

    minimal refactor

---
+++ pracs/prac1/python/frames.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+
+def non_overlapping_frames(signal, fs, frame_duration):
+    frame_len = round(frame_duration * fs)
+    total_frames = len(signal) // frame_len
+
+    frames = signal[: total_frames * frame_len]
+    frames = frames.reshape(frame_len, total_frames, order="F")
+
+    return frames
+
+
+def overlapping_frames(signal, fs, frame_duration, overlap):
+    frame_len = round(frame_duration * fs)
+    hop_len = round(frame_len * (1 - overlap))
+    total_frames = 1 + (len(signal) - frame_len) // hop_len
+
+    frames = np.zeros((frame_len, total_frames))
+    for i in range(total_frames):
+        start = i * hop_len
+        end = start + frame_len
+        frames[:, i] = signal[start:end]
+
+    return frames
---
+++ pracs/prac1/python/plot.py
@@ -0,0 +1,39 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def plot_continuous(signal, fs, title=None, ylabel=None):
+    t = np.arange(len(signal)) / fs
+
+    plt.plot(t, signal)
+    plt.xlabel("Time (s)")
+
+    if title:
+        plt.title(title)
+
+    if ylabel:
+        plt.ylabel(ylabel)
+
+    plt.show()
+
+
+# plot both continuous and discrete signal in the same figure
+def plot_continuous_discrete(signal, fs, title=None):
+    Ly = len(signal)
+    t = np.arange(Ly) / fs
+    n = np.arange(Ly)
+
+    fig, ax = plt.subplots(2, layout="constrained")
+
+    ax[0].plot(t, signal)
+    ax[0].set_xlabel("Time (s)")
+    ax[0].set_title("Continuous")
+
+    ax[1].plot(n, signal)
+    ax[1].set_xlabel("n")
+    ax[1].set_title("Discrete")
+
+    if title:
+        fig.suptitle(title)
+
+    plt.show()
